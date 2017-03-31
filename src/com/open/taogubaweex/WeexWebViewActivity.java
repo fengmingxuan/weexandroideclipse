130d75d2bced4eb34faa6041f41c94175d10f9da
fengmnegchang <624926379@qq.com>
2017-03-31 14:58:48 +0800

    webview缓存

--- src/com/open/taogubaweex/WeexWebViewActivity.java
+++ src/com/open/taogubaweex/WeexWebViewActivity.java
@@ -11,6 +11,8 @@
  */
 package com.open.taogubaweex;
 
+import java.io.File;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -21,12 +23,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.LayoutAlgorithm;
+import android.webkit.WebSettings.RenderPriority;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.open.taogubaweex.utils.WeexUtils;
 
@@ -46,7 +52,7 @@ public class WeexWebViewActivity extends  Activity {
 	public static final String TAG = WeexWebViewActivity.class.getSimpleName();
 	public WebView webview;
 	public String url = WeexUtils.HTTP+"://"+WeexUtils.IP+WeexUtils.MAIN_JS;
-
+	private static final String APP_CACAHE_DIRNAME = "/webcache";  
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -98,6 +104,23 @@ public class WeexWebViewActivity extends  Activity {
 		}  
 		webSettings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
 		webSettings.setLoadWithOverviewMode(true);
+		
+		webSettings.setRenderPriority(RenderPriority.HIGH);  
+		webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);  //设置 缓存模式  
+        // 开启 DOM storage API 功能  
+		webSettings.setDomStorageEnabled(true);  
+        //开启 database storage API 功能  
+		webSettings.setDatabaseEnabled(true);   
+        String cacheDirPath = getFilesDir().getAbsolutePath()+APP_CACAHE_DIRNAME;  
+//      String cacheDirPath = getCacheDir().getAbsolutePath()+Constant.APP_DB_DIRNAME;  
+        Log.i(TAG, "cacheDirPath="+cacheDirPath);  
+        //设置数据库缓存路径  
+        webSettings.setDatabasePath(cacheDirPath);  
+        //设置  Application Caches 缓存目录  
+        webSettings.setAppCachePath(cacheDirPath);  
+        //开启 Application Caches 功能  
+        webSettings.setAppCacheEnabled(true);  
+		
 		if (getIntent().getStringExtra("URL") != null) {
 			url = getIntent().getStringExtra("URL");
 		}
@@ -174,6 +197,26 @@ public class WeexWebViewActivity extends  Activity {
 			// TODO Auto-generated method stub
 			return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
 		}
+		
+		@Override  
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {  
+            Log.e(TAG, "onJsAlert " + message);  
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();  
+            result.confirm();  
+            return true;  
+        }  
+
+        @Override  
+        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {  
+            Log.e(TAG, "onJsConfirm " + message);  
+            return super.onJsConfirm(view, url, message, result);  
+        }  
+
+        @Override  
+        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {  
+            Log.e(TAG, "onJsPrompt " + url);  
+            return super.onJsPrompt(view, url, message, defaultValue, result);  
+        }  
 
 	}
 
@@ -191,6 +234,56 @@ public class WeexWebViewActivity extends  Activity {
 			super.onBackPressed();
 		}
 	}
+	
+	/** 
+     * 清除WebView缓存 
+     */  
+    public void clearWebViewCache(){  
+        //清理Webview缓存数据库  
+        try {  
+            deleteDatabase("webview.db");   
+            deleteDatabase("webviewCache.db");  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        //WebView 缓存文件  
+        File appCacheDir = new File(getFilesDir().getAbsolutePath()+APP_CACAHE_DIRNAME);  
+        Log.e(TAG, "appCacheDir path="+appCacheDir.getAbsolutePath());  
+          
+        File webviewCacheDir = new File(getCacheDir().getAbsolutePath()+"/webviewCache");  
+        Log.e(TAG, "webviewCacheDir path="+webviewCacheDir.getAbsolutePath());  
+          
+        //删除webview 缓存目录  
+        if(webviewCacheDir.exists()){  
+            deleteFile(webviewCacheDir);  
+        }  
+        //删除webview 缓存 缓存目录  
+        if(appCacheDir.exists()){  
+            deleteFile(appCacheDir);  
+        }  
+    }  
+      
+    /** 
+     * 递归删除 文件/文件夹 
+     *  
+     * @param file 
+     */  
+    public void deleteFile(File file) {  
+        Log.i(TAG, "delete file path=" + file.getAbsolutePath());  
+        if (file.exists()) {  
+            if (file.isFile()) {  
+                file.delete();  
+            } else if (file.isDirectory()) {  
+                File files[] = file.listFiles();  
+                for (int i = 0; i < files.length; i++) {  
+                    deleteFile(files[i]);  
+                }  
+            }  
+            file.delete();  
+        } else {  
+            Log.e(TAG, "delete file no exists " + file.getAbsolutePath());  
+        }  
+    }  
 
 	public static void startUmeiWebViewActivity(Context context, String url) {
 		Intent intent = new Intent();
